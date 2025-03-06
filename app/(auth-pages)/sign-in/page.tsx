ea14d82a95254cc65df0cc2bb83b1b98d94cdfa6
Tjhonnark <tjhonnar.gmail.com>
2025-03-06 00:59:51 -0500

    0.4.5

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,4 +1,4 @@
-import { signInAction } from "@/app/actions";
+import { signInAction, signInWithGoogleAction } from "@/app/actions";
 import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/auth/submit-button";
 import { Input } from "@/components/ui/input";
@@ -12,6 +12,24 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
+
+export function ButtonGoogle() {
+  return (
+    <button className="w-full cursor-pointer text-black flex gap-2 justify-center items-center bg-white px-4 py-2 rounded-lg font-medium text-sm hover:bg-zinc-300 transition-all ease-in duration-200"
+    onClick={signInWithGoogleAction}
+    >
+      <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" className="w-6">
+        <path d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" fill="#FFC107" />
+        <path d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" fill="#FF3D00" />
+        <path d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z" fill="#4CAF50" />
+        <path d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z" fill="#1976D2" />
+      </svg>
+      Continue with Google
+    </button>
+  );
+}
+
+
 
 export default async function Login(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
@@ -29,7 +47,7 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
             </Link>
         </CardDescription>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex flex-col gap-4">
         <form className="flex-1 flex flex-col min-w-64">
           <div className="flex flex-col gap-2 [&>input]:mb-3 mt-3">
             <Label htmlFor="email">Email</Label>
@@ -54,6 +72,7 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
           Sign in
         </SubmitButton>
         </form>
+        <ButtonGoogle />
       </CardContent>
       <CardFooter className="flex justify-between">
         <FormMessage message={searchParams} />
--- app/actions.ts
+++ app/actions.ts
@@ -139,3 +139,18 @@ export const signOutAction = async () => {
   await supabase.auth.signOut();
   return redirect("/sign-in");
 };
+
+export const signInWithGoogleAction = async () => {
+  const supabase = await createClient();
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`,
+    },
+  });
+  if (error) {
+    console.error(error.message);
+    return encodedRedirect("error", "/sign-in", error.message);
+  }
+  return redirect("/dashboard");
+};
