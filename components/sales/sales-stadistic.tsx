a948d47902ba6546a7685b2d90fdf817f74c94c6
7hodev <tjhonnar@gmail.com>
2025-03-08 18:52:17 -0500

    0.5.9

--- components/sales/sales-stadistic.tsx
+++ components/sales/sales-stadistic.tsx
@@ -118,7 +118,7 @@ export default function SalesStadistic() {
   };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-4">
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Ventas Diarias</CardTitle>
